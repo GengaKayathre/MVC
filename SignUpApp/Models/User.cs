5e80384cb72dd76715444c2befbf3c2206265083
GengaKayathre <94345877+GengaKayathre@users.noreply.github.com>
2024-04-22 12:06:39 +0530

    UI changes are in progress

--- SignUpApp/Models/User.cs
+++ SignUpApp/Models/User.cs
@@ -4,14 +4,17 @@ namespace SignUpApp.Models
 {
     public class User
     {
-        [Required(ErrorMessage = "Username is required")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]        
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
         public string Email { get; set; }
     }
 }
